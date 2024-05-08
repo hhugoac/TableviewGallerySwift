5c5d0b4c0efdc7082f5859202c3ae68e6b54bd01
Hugo Alonzo <hugoalonzo@iMac-de-Hugo.local>
2024-05-07 23:39:22 -0600

    In this commit the grid buttuns was setup

--- TableViewGallery/06CalculatorCollectionView/CalculatorViewController.swift
+++ TableViewGallery/06CalculatorCollectionView/CalculatorViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var calculatorCollectionView: UICollectionView!
+    private let spacing:CGFloat = 16.0
     
     let colorData = [ #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1) , #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1) , #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) , #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1) , #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1) , #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1) ,
                           #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1) , #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1) , #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) , #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1) , #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1) , #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1) ,
@@ -21,7 +22,7 @@ class CalculatorViewController: UIViewController {
         "7", "8", "9", "x",
         "4", "5", "6", "-",
         "1", "2", "3", "+",
-        "0", ".", "="
+        "", "0", ".", "="
     ]
     
     private var collectionViewHeightConstraint: NSLayoutConstraint!
@@ -30,13 +31,18 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
         calculatorCollectionView.delegate = self
         calculatorCollectionView.dataSource = self
-        collectionViewHeightConstraint = collectionView.heightAnchor.constraint(equalToConstant: 50)
+        let layout = UICollectionViewFlowLayout()
+                layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+                layout.minimumLineSpacing = spacing
+                calculatorCollectionView.collectionViewLayout = layout
         calculatorCollectionView.isScrollEnabled = false
     }
 
 }
 
 extension CalculatorViewController: UICollectionViewDataSource {
+    
+    
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
             let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header", for: indexPath as IndexPath)
             return headerView
@@ -62,18 +68,19 @@ extension CalculatorViewController: UICollectionViewDelegate {
 
 extension CalculatorViewController: UICollectionViewDelegateFlowLayout {
 
-    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let columns = 4
-
-        //spaicing
-        let spacing: CGFloat = 5
-        let totalHorizontalSpaicing = (columns -1) * spacing
+        let numberOfItemsPerRow:CGFloat = 4
+        let spacingBetweenCells:CGFloat = 16
+               
+        let totalSpacing = (2 * self.spacing) + ((numberOfItemsPerRow - 1) * spacingBetweenCells) //Amount of total spacing in a row
+               
         
-        let itemWidth = (collectionView.bounds.width - totalHorizontalSpaicing) / columns
-        let itemSize = CGSize(width: itemWidth, height: itemWidth)
-        
-        return itemSize
+        let width = (calculatorCollectionView.bounds.width - totalSpacing)/numberOfItemsPerRow
+        return CGSize(width: width, height: width)
+       
     }
     
 }
